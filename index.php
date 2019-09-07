27b90cffe1657d01280294e21c4f8f3d4ec08b6c
Junitos <jmlu2512@gmail.com>
2019-09-07 15:41:50 -0300

    esqueci de tirar o comflito

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php 
-
-<<<<<<< HEAD
-echo "AEWWWW  465"
-echo "Foi da maquina local "
-=======
-echo "AEWWWW 123"
-echo "Vishhhh"
->>>>>>> 6985fae8fa58d21862f1948572ebeb5b8bbba927
-
+echo "AEWWWW  465";
+echo "Foi da maquina local ";
 ?>
